4e70ea0cff756e3784b186175665052fc4fb1bee
alienzhou <alienzhou16@163.com>
2016-10-12 22:48:46 +0800

    添加combobox组件（单选）

---
+++ app/components/combobox.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import '../less/alien-ui-combobox.less';
+import PubSub from 'pubsub-js';
+
+class ComboItem extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		var classVal=this.props.item.chosen?'active':'';
+		return <li data-alienkey={this.props.item.alienKey} className={classVal}>{this.props.item.text}</li>
+	}
+}
+
+class ComboList extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			items:props.items
+		};
+	}
+	selectItem(e){
+		var obj=e.target;
+		if(obj.tagName.toLowerCase()=='li'){
+			this.props.callbackSelect(obj.dataset.alienkey);
+		}
+	}
+	render(){
+		return <ul className='combobox-panel' onClick={(e)=>this.selectItem(e)}>
+			{
+				this.state.items.map((item,index)=>{
+					return <ComboItem key={item.alienKey} item={item} />
+				})
+			}
+		</ul>
+	}
+}
+
+class ComboInput extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return (
+			<div className='input-area'>
+				<span className='toggle' onClick={this.props.callbackParent}></span>
+				<input value={this.props.inputVal} />
+			</div>
+		)
+	}
+}
+
+export default class Combobox extends React.Component {
+	constructor(props){
+		props.combodata.forEach((v,i)=>{
+			v.alienKey=i;
+		});
+		
+		super(props);
+		this.state={
+			open:false,
+			items:props.combodata
+		}
+		document.body.addEventListener('click',(e)=>{
+			if(e.target!=this.combobox){
+				this.setState({
+					open:false
+				});
+			}
+		});
+	}
+	togglePanel(){
+		this.state.open=!this.state.open;
+		this.setState(this.state);
+	}
+	changeText(chosenKey){
+		this.state.items.forEach((v,i)=>{
+			v.chosen=false;
+			if(chosenKey==v.alienKey){
+				v.chosen=true;
+			}
+		});
+		this.setState(this.state);
+	}
+	render(){
+		var className=this.state.open?'alien-combobox open':'alien-combobox';
+		var chosenVal=this.state.items.filter((v,i)=>{
+			return v.chosen==true;
+			v.chosen=false;
+		});
+		return (
+			<div ref={(ref)=>{this.combobox=ref}} className={className}>
+				<ComboInput callbackParent={()=>{this.togglePanel()}} inputVal={chosenVal[0].text} />
+				<ComboList items={this.state.items} callbackSelect={(chosenKey)=>this.changeText(chosenKey)} />
+			</div>
+		)
+	}
+}
--- app/entry.js
+++ app/entry.js
@@ -3,11 +3,40 @@ import ReactDOM from 'react-dom';
 import Radio from './components/radio.jsx';
 import Header from './components/header.jsx';
 import Textbox from './components/textbox.jsx';
+import Combobox from './components/combobox.jsx';
+
+var comboboxData = [{
+	key: 1,
+	text: '与我常在',
+	val: 1,
+	chosen: false
+}, {
+	key: 2,
+	text: '路过蜻蜓',
+	val: 2,
+	chosen: false
+}, {
+	key: 3,
+	text: '当爱已成往事',
+	val: 3,
+	chosen: true
+}, {
+	key: 4,
+	text: '陀飞轮',
+	val: 4,
+	chosen: false
+}, {
+	key: 5,
+	text: '红',
+	val: 5,
+	chosen: false
+}];
 
 main();
 
 function main() {
-	ReactDOM.render(<Radio /> , document.getElementById('radioApp'));
-	ReactDOM.render(<Header /> , document.getElementById('headerApp'));
-	ReactDOM.render(<Textbox /> , document.getElementById('textboxApp'));
+	ReactDOM.render( <Radio /> , document.getElementById('radioApp'));
+	ReactDOM.render( <Header /> , document.getElementById('headerApp'));
+	ReactDOM.render( <Textbox /> , document.getElementById('textboxApp'));
+	ReactDOM.render( <Combobox combodata={comboboxData} /> , document.getElementById('comboboxApp'));
 }
